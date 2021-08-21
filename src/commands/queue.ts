dc274644a7073f59f47bddbe1e7feabd3e945dd4
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-08-21 21:42:41 +0200

    feat(queue): added /queue command

---
+++ src/commands/queue.ts
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import { EmbedField, MessageOptions, SlashCommand, SlashCreator } from 'slash-create';
+import { config } from '../util/config';
+
+export class QueueCommand extends SlashCommand {
+  constructor(creator: SlashCreator) {
+    super(creator, {
+      name: 'queue',
+      description: 'Check the lengths of the import queues',
+      guildIDs: config.discord.guildId
+    });
+  }
+
+  async run(): Promise<string | MessageOptions | void> {
+    type Type = {
+      aggregations: {
+        // eslint-disable-next-line camelcase
+        total_streams: {
+          value: number;
+        };
+        queue: {
+          buckets: {
+            key: number;
+            // eslint-disable-next-line camelcase
+            doc_count: number;
+          }[];
+        };
+      };
+      _shards: {
+        total: number;
+        successful: number;
+        skipped: number;
+        failed: number;
+      };
+    };
+    const res: Type = await axios.get(`${config.api.StatsURL}/elastic/queue/agg`);
+    const embedFields: EmbedField[] = [];
+
+    res.aggregations.queue.buckets.forEach((server) => {
+      const name = [
+        'Ernie.spotistats.app',
+        'Purk.spotistats.app',
+        'Tommie.spotistats.app',
+        'Aart.spotistats.app'
+      ][server.key];
+
+      embedFields.push({
+        name,
+        value: `${server.doc_count} files in queue`
+      });
+    });
+
+    return {
+      embeds: [
+        {
+          title: '<:spotistats:814651268907401256> Spotistats Status',
+          fields: embedFields,
+          color: 0x1db954
+        }
+      ]
+    };
+  }
+}
