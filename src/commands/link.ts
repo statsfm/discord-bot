0a4b5e15f5da4be4ba88590204156dc6093fa368
Stijn van der Kolk <stijn@kolkies.dev>
2022-03-18 21:28:32 +0000

    feat: add new linking method

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -1,111 +1,51 @@
-import {
-  CommandContext,
-  CommandOptionType,
-  SlashCommand,
-  SlashCreator,
-  MessageOptions
-} from 'slash-create';
-import { client, prisma, spotistats, spotistatsBeta } from '..';
+import { CommandContext, SlashCommand, SlashCreator, MessageOptions } from 'slash-create';
+import StringCrypto from 'string-crypto';
+import { prisma } from '..';
 import { config } from '../util/config';
+
+const cryptoGen = new StringCrypto({
+  salt: process.env.SECRET_KEY,
+  iterations: 10,
+  digest: 'sha3-512'
+});
 
 export default class LinkCommand extends SlashCommand {
   constructor(creator: SlashCreator) {
     super(creator, {
       name: 'link',
       description: 'Link your Spotistats account with Discord and gain access to special roles',
-      guildIDs: config.discord.guildId,
-      options: [
-        {
-          type: CommandOptionType.STRING,
-          name: 'code',
-          description: 'The import code shown on the import settings in the Spotistats app',
-          required: true
-        }
-      ]
+      guildIDs: config.discord.guildId
     });
   }
 
   async run(ctx: CommandContext): Promise<string | MessageOptions | void> {
-    try {
-      let account = await prisma.account.findUnique({
-        where: { discordUserId: ctx.user.id }
-      });
+    const account = await prisma.account.findUnique({
+      where: { discordUserId: ctx.user.id }
+    });
 
-      if (account) {
-        return {
-          content:
-            'You already have an Spotistats account linked, please unlink it first with `/unlink`'
-          // ephemeral: true,
-        };
-      }
-
-      const code = (ctx.options.code as string).toUpperCase();
-      if (code.length !== 6) {
-        return 'Invalid import code :(';
-      }
-
-      let isBeta = false;
-      let hasPlusInBeta = false;
-      let res = await spotistats.getUserDataFromCode(code);
-      if (!res.status) {
-        res = await spotistatsBeta.getUserDataFromCode(code);
-
-        if (!res.status || !res.data) {
-          return { content: 'Invalid import code :(' }; // ephemeral: true
-        }
-
-        isBeta = true;
-        hasPlusInBeta = res.data.isPlus;
-
-        res = await spotistats.getUserDataFromId(res.data.id);
-      }
-
-      account = await prisma.account.findUnique({
-        where: { spotistatsUserId: res.data.id }
-      });
-      if (account) {
-        return {
-          content: `${res.data.displayName}'s Spotistats account has already been linked to <@!${account.discordUserId}>. Ask <@!${account.discordUserId}> to unlink it with \`/unlink\`.`,
-          allowedMentions: { everyone: false, users: [] }
-          // ephemeral: true,
-        };
-      }
-
-      const content = [
-        `Successfully linked your Spotistats account (${res.data.displayName}) to your Discord account!`
-      ];
-      if (res.data.isPlus) {
-        const member = client.guilds.resolve(ctx.guildID).members.resolve(ctx.user.id);
-        await member.roles.add(config.discord.roles.plus);
-        content.push(`Added the <@&${config.discord.roles.plus}> role :)`);
-      } else if (hasPlusInBeta) {
-        content.push(
-          `It looks like you only have Plus in the beta app, so you haven't received the <@&${config.discord.roles.plus}> role :(`
-        );
-      }
-      if (isBeta) {
-        const member = client.guilds.resolve(ctx.guildID).members.resolve(ctx.user.id);
-        await member.roles.add(config.discord.roles.beta);
-        content.push(
-          `Added the <@&${config.discord.roles.beta}> role, thanks for being a beta tester!`
-        );
-      }
-
-      await prisma.account.create({
-        data: { discordUserId: ctx.user.id, spotistatsUserId: res.data.id }
-      });
-
+    if (account) {
       return {
-        content: content.join('\n'),
+        content:
+          'You already have an Spotistats account linked, please unlink it first with `/unlink`'
         // ephemeral: true,
-        allowedMentions: {
-          everyone: false,
-          roles: []
-        }
       };
-    } catch (error) {
-      console.log(error);
-      throw new Error(error);
     }
+
+    const link = `https://api.stats.fm/api/v1/auth/redirect/spotify?scope=user-read-private&redirect_uri=https://discord-bot.stats.fm/callback&state=${cryptoGen.encryptString(
+      ctx.user.id,
+      process.env.PASSWORD
+    )}`;
+
+    return {
+      ephemeral: true,
+      embeds: [
+        {
+          author: {
+            name: 'Spotistats Account Linking'
+          },
+          description: `To be able to link your account, please click [here](${link}). After linking, to obtain your Spotistats Plus role, please use the command \`/plus\`.`
+        }
+      ]
+    };
   }
 }
