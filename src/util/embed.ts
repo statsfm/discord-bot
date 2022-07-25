c9f39f7534ba48c37619230d8cc17f882c1b73ea
Stijn van der Kolk <stijn@kolkies.dev>
2022-07-25 00:10:22 -0400

    feat: upgrade embeds

--- src/util/embed.ts
+++ src/util/embed.ts
@@ -37,12 +37,15 @@ export const notLinkedEmbed = (issuer: APIUser, targetUser: APIUser) =>
     .setTitle(
       `${targetUser.username} did not link their Discord account to their Stats.fm account`
     )
+    .setDescription(
+      'You can link your Discord account to your Stats.fm account by going to <https://stats.fm/account/connections>!'
+    )
     .toJSON();
 
 export const unexpectedErrorEmbed = (issuer: APIUser, targetUser: APIUser) =>
   createEmbed(issuer)
     .setTitle('An unexpected error occurred')
     .setDescription(
-      `**${targetUser.username}** probably has set some privacy settings to private, if you're sure they've not, please contact bot support.`
+      `**${targetUser.username}** probably has set some [privacy settings](https://stats.fm/account/privacy) to private, if you're sure they've not, please contact bot support.`
     )
     .toJSON();
