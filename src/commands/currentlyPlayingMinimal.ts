bfb8106e4f7fb8dd8264089afca93e63516b06e7
Stijn van der Kolk <stijn@kolkies.dev>
2023-03-28 23:22:23 +0200

    feat: experiment finished

--- src/commands/currentlyPlayingMinimal.ts
+++ src/commands/currentlyPlayingMinimal.ts
@@ -18,7 +18,6 @@ import {
 import { getStatsfmUserFromDiscordUser } from '../util/getStatsfmUserFromDiscordUser';
 import { reportError } from '../util/Sentry';
 import { URLs } from '../util/URLs';
-import { murmur3 } from 'murmurhash-js';
 import { getDuration } from '../util/getDuration';
 import { PrivacyManager } from '../util/PrivacyManager';
 import { CooldownManager } from '../util/CooldownManager';
@@ -84,6 +83,7 @@ export default createCommand(CurrentlyPlayingMinimalCommand)
         }
       }
     } else {
+      cooldownManager.clear(interaction.commandName, interaction.user.id);
       return respond(interaction, {
         embeds: [
           privacyEmbed(
@@ -103,13 +103,6 @@ export default createCommand(CurrentlyPlayingMinimalCommand)
       });
     }
 
-    const statsEmbedGroup: [number, number] = [0, 0];
-    const noStatsEmbedGroup: [number, number] = [0, 0];
-    const experimentHash =
-      murmur3(
-        `03-2023-now_playing_minimal|${targetUser.id}|${showStats ? '1' : '0'}`
-      ) % 1e3;
-
     let stats: StreamStats | undefined;
     if (statsfmUser.privacySettings.streamStats && showStats) {
       try {
@@ -121,10 +114,19 @@ export default createCommand(CurrentlyPlayingMinimalCommand)
           }
         );
       } catch (err) {
-        const errorId = reportError(err, interaction);
-        return respond(interaction, {
-          embeds: [unexpectedErrorEmbed(errorId)],
-        });
+        stats = await statsfmApi.users
+          .trackStats(statsfmUser.id, currentlyPlaying.track.id, {
+            range: Range.LIFETIME,
+          })
+          .catch(() => undefined);
+        if (!stats) {
+          const errorId = reportError(err, interaction);
+          cooldownManager.clear(interaction.commandName, interaction.user.id);
+
+          return respond(interaction, {
+            embeds: [unexpectedErrorEmbed(errorId)],
+          });
+        }
       }
     } else if (!statsfmUser.privacySettings.streamStats && showStats) {
       return respond(interaction, {
@@ -146,66 +148,32 @@ export default createCommand(CurrentlyPlayingMinimalCommand)
       .map((artist) => `**[${artist.name}](${URLs.ArtistUrl(artist.id)})**`)
       .join(', ')}`;
 
-    const defaultTextMessage = `**${targetUser.tag}** is currently listening to ${songByArtist}.`;
-
-    const embed = createEmbed()
-      .setAuthor({
-        name: `${targetUser.tag} is currently listening to`,
-        iconURL: targetUser.displayAvatarURL(),
-      })
-      .setDescription(songByArtist)
-      .setTimestamp()
-      .setThumbnail(currentlyPlaying.track.albums[0].image);
 
     if (
-      isInExperimentGroup(experimentHash, [statsEmbedGroup]) &&
       showStats &&
       stats
     ) {
-      embed.setFooter({
-        text: `Lifetime streams: ${stats.count ?? 0} • Total time streamed: ${
-          stats.durationMs > 0
+      const embed = createEmbed()
+        .setAuthor({
+          name: `${targetUser.tag} is currently listening to`,
+          iconURL: targetUser.displayAvatarURL(),
+        })
+        .setDescription(songByArtist)
+        .setTimestamp()
+        .setThumbnail(currentlyPlaying.track.albums[0].image).setFooter({
+          text: `Lifetime streams: ${stats.count ?? 0} • Total time streamed: ${stats.durationMs > 0
             ? getDuration(stats.durationMs, true)
             : '0 minutes'
-        }`,
-      });
+            }`,
+        });
 
       return respond(interaction, {
         embeds: [embed],
       });
-    } else if (
-      !isInExperimentGroup(experimentHash, [statsEmbedGroup]) &&
-      showStats &&
-      stats
-    ) {
-      return respond(interaction, {
-        content: `${defaultTextMessage} **${
-          stats.count ?? 0
-        }** lifetime streams and ${
-          stats.durationMs > 0 ? getDuration(stats.durationMs) : '**0** minutes'
-        } total time streamed.`,
-        flags: MessageFlags.SuppressEmbeds,
-      });
     }
-
-    if (isInExperimentGroup(experimentHash, [noStatsEmbedGroup])) {
-      return respond(interaction, {
-        embeds: [embed],
-      });
-    } else {
-      return respond(interaction, {
-        content: defaultTextMessage,
-        flags: MessageFlags.SuppressEmbeds,
-      });
-    }
+    return respond(interaction, {
+      content: `**${targetUser.tag}** is currently listening to ${songByArtist}.`,
+      flags: MessageFlags.SuppressEmbeds,
+    });
   })
   .build();
-
-function isInExperimentGroup(
-  experimentHash: number,
-  groups: [number, number][]
-) {
-  return groups.some(
-    (group) => experimentHash >= group[0] && experimentHash < group[1]
-  );
-}
