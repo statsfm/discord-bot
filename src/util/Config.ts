defabdbb3e6abbe7f97e56c6dfca09253c6ee620
Stijn van der Kolk <stijn@kolkies.dev>
2024-01-25 12:20:57 +0100

    feat(config): move away from toml

--- src/util/Config.ts
+++ src/util/Config.ts
@@ -1,8 +1,4 @@
 import { inject, singleton } from 'tsyringe';
-import toml from 'toml';
-import fs from 'node:fs';
-import path from 'node:path';
-import type { AnalyticsConfig, IConfig } from './IConfig';
 import ow, { ArgumentError } from 'ow';
 import type { Options } from '@statsfm/statsfm.js';
 import type { Snowflake } from 'discord-api-types/globals';
@@ -11,71 +7,82 @@ import { kLogger } from './tokens';
 import * as Sentry from '@sentry/node';
 
 @singleton()
-export class Config implements IConfig {
+export class Config {
   statsfm: Options;
   discordBotToken: string;
   discordClientId: Snowflake;
   sentryDsn: string;
-  analytics: AnalyticsConfig | undefined;
+  analytics:
+    | {
+        token: string;
+        fileLocation: string;
+      }
+    | undefined;
 
   // Setup all config things from the toml in class
   constructor(@inject(kLogger) public readonly logger: Logger) {
-    const file = fs.readFileSync(
-      path.resolve(__dirname, '..', '..', 'config.toml'),
-      { encoding: 'utf-8' }
-    );
-    const tomlConfig: IConfig = toml.parse(file);
     try {
-      this.tomlCheck(tomlConfig);
+      this.envCheck();
     } catch (e) {
       Sentry.captureException(e);
       const error = e as ArgumentError;
       logger.error(error.message);
       process.exit(1);
     }
-    this.statsfm = tomlConfig.statsfm;
-    this.analytics = tomlConfig.analytics;
+    this.statsfm = {
+      http: {
+        apiUrl: process.env.STATSFM_HTTP_API_URL!,
+        userAgentAppendix: process.env.STATSFM_HTTP_API_USER_AGENT_APPENDIX!,
+        retries: Number(process.env.STATSFM_HTTP_API_RETRIES!),
+        version: process.env.STATSFM_HTTP_API_VERSION!,
+      },
+      auth: {
+        accessToken: process.env.STATSFM_AUTH_ACCESS_TOKEN!,
+      },
+    };
+    this.analytics =
+      process.env.ANALYTICS_TOKEN && process.env.ANALYTICS_FILE_LOCATION
+        ? {
+            token: process.env.ANALYTICS_TOKEN!,
+            fileLocation: process.env.ANALYTICS_FILE_LOCATION!,
+          }
+        : undefined;
     this.discordBotToken = process.env.DISCORD_BOT_TOKEN!;
     this.discordClientId = process.env.DISCORD_CLIENT_ID!;
     this.sentryDsn = process.env.SENTRY_DSN!;
   }
 
-  private tomlCheck(tomlConfig: IConfig) {
+  private envCheck() {
     ow(process.env.DISCORD_BOT_TOKEN!, ow.string.nonEmpty);
     ow(process.env.DISCORD_CLIENT_ID!, ow.string.nonEmpty);
-    ow(process.env.SENTRY_DSN!, ow.optional.string.nonEmpty);
+    ow(process.env.SENTRY_DSN!, ow.any(ow.nullOrUndefined, ow.string.nonEmpty));
     ow(
-      tomlConfig.statsfm,
-      ow.any(
-        ow.nullOrUndefined,
-        ow.object.exactShape({
-          http: ow.any(
-            ow.nullOrUndefined,
-            ow.object.exactShape({
-              apiUrl: ow.any(ow.nullOrUndefined, ow.string.nonEmpty),
-              userAgentAppendix: ow.any(ow.nullOrUndefined, ow.string.nonEmpty),
-              retries: ow.any(ow.nullOrUndefined, ow.number),
-              version: ow.any(ow.nullOrUndefined, ow.string.nonEmpty),
-            })
-          ),
-          auth: ow.any(
-            ow.nullOrUndefined,
-            ow.object.exactShape({
-              accessToken: ow.any(ow.nullOrUndefined, ow.string.nonEmpty),
-            })
-          ),
-        })
-      )
+      process.env.STATSFM_HTTP_API_URL!,
+      ow.any(ow.nullOrUndefined, ow.string.nonEmpty)
     );
     ow(
-      tomlConfig.analytics,
-      ow.any(
-        ow.nullOrUndefined,
-        ow.object.exactShape({
-          token: ow.string.nonEmpty,
-          fileLocation: ow.string.nonEmpty,
-        })
-      )
+      process.env.STATSFM_HTTP_API_USER_AGENT_APPENDIX!,
+      ow.any(ow.nullOrUndefined, ow.string.nonEmpty)
+    );
+    ow(
+      process.env.STATSFM_HTTP_API_RETRIES!,
+      ow.any(ow.nullOrUndefined, ow.number)
+    );
+    ow(
+      process.env.STATSFM_HTTP_API_VERSION!,
+      ow.any(ow.nullOrUndefined, ow.string.nonEmpty)
+    );
+    ow(
+      process.env.STATSFM_AUTH_ACCESS_TOKEN!,
+      ow.any(ow.nullOrUndefined, ow.string.nonEmpty)
+    );
+    ow(
+      process.env.ANALYTICS_TOKEN!,
+      ow.any(ow.nullOrUndefined, ow.string.nonEmpty)
+    );
+    ow(
+      process.env.ANALYTICS_FILE_LOCATION!,
+      ow.any(ow.nullOrUndefined, ow.string.nonEmpty)
     );
   }
 }
