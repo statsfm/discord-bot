e6ace27a5f9b7f562da1e48682b3a52211312ab2
Stijn van der Kolk <stijn@kolkies.dev>
2024-07-10 22:58:11 +0200

    log shards

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -7,6 +7,10 @@ const logger = container.resolve<Logger>(kLogger);
 
 export default createEvent('ready')
   .setOn(async () => {
-    logger.info('Online!');
+    logger.info(
+      `Online with the following shards ${
+        process.env.SHARDS?.split(',').map((s) => parseInt(s)) || [0].join(', ')
+      }`
+    );
   })
   .build();
